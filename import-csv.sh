c6c5451df60c151860b0331db213d3abd09058ec
Vladimir <vladimir@pravdabox.net>
2017-11-01 11:17:00 +0100

    november 2017 db and importscript

--- import-csv.sh
+++ import-csv.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-[[ -f IP2LOCATION-LITE-DB5.CSV.ZIP ]] || echo 'Download and copy IP2LOCATION-LITE-DB5.CSV.ZIP to this directory first! (https://lite.ip2location.com/file-download)' ; exit 1
+if [[ ! -f IP2LOCATION-LITE-DB5.CSV.ZIP ]]; then
+    echo 'Download and copy IP2LOCATION-LITE-DB5.CSV.ZIP to this directory first! (https://lite.ip2location.com/file-download)'
+    exit 1
+fi
 
+# extract
 unzip IP2LOCATION-LITE-DB5.CSV.ZIP
+echo
+
+echo "running import..."
+mysql -u root --password=root --local-infile ip2location < import-csv.sql
+
+echo "running export..."
+mysqldump -u root --password=root ip2location > geolocation/files/ip2location_db5.sql
+
+echo "compressing..."
+rm geolocation/files/ip2location_db5.sql.xz
+xz -9e geolocation/files/ip2location_db5.sql
+
+echo "done."
+
+# cleanup
+rm LICENSE_LITE.TXT
+rm README_LITE.TXT
+rm IP2LOCATION-LITE-DB5.CSV
